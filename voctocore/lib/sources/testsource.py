e8adc80c49a455c442e626b63b0ae145c8b0c2c7
Carl Karsten <carl@personnelware.com>
2026-04-05 13:22:41 -0500

    revert changes to testsource.py.  squish away.

--- voctocore/lib/sources/testsource.py
+++ voctocore/lib/sources/testsource.py
@@ -15,40 +15,50 @@ class TestSource(AVSource):
                          force_num_streams)
 
         self.name = name
-        self.video_source = Config.getGstVideoPipe(name)
-        self.audio_source = Config.getGstAudioPipe(name)
+        self.pattern = Config.getTestPattern(name)
+        self.wave = Config.getTestWave(name)
         self.build_pipeline()
 
     def port(self):
-        return "GST AV"
+        if self.has_video:
+            if self.internal_audio_channels():
+                return "(AV:{}+{})".format(self.pattern, self.wave)
+            else:
+                return "(V:{})".format(self.pattern)
+        else:
+            if self.internal_audio_channels():
+                return "(A:{})".format(self.wave)
+        return "Test"
 
     def num_connections(self):
         return 1
 
     def __str__(self):
-        return 'GstSource[{name}]'.format(
+        return 'TestSource[{name}] ({pattern}, {wave})'.format(
             name=self.name,
+            pattern=self.pattern,
+            wave=self.wave
         )
 
     def build_audioport(self):
-        return """{audio_source}
-                      name=gstaudiosrc-{name}
-                      """.format(
-            audio_source=self.audio_source,
+        # a volume of 0.126 is ~18dBFS
+        return """audiotestsrc
+                      name=audiotestsrc-{name}
+                      do-timestamp=true
+                      freq=1000
+                      volume=0.126
+                      wave={wave}
+                      is-live=true""".format(
             name=self.name,
+            wave=self.wave,
         )
 
     def build_videoport(self):
-
-        return """{video_source}
-                      name=gstvideosrc-{name}
-                ! queue max-size-time=4000000000
-                ! videoconvert
-                ! videorate
-                ! videoscale
-                ! queue max-size-time=4000000000
-                      """.format(
-            video_source=self.video_source,
+        return """videotestsrc
+                      name=videotestsrc-{name}
+                      do-timestamp=true
+                      pattern={pattern}
+                      is-live=true""".format(
             name=self.name,
+            pattern=self.pattern
         )
- 
