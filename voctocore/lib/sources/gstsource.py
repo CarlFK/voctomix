541f7fbdce2ec03ff5fe7a706f484cf7a6c19582
Carl Karsten <carl@personnelware.com>
2026-04-04 20:33:05 -0500

    add gstsource - define gstreamer sources in -i core.ini

--- voctocore/lib/sources/gstsource.py
+++ voctocore/lib/sources/gstsource.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import logging
 
-from configparser import NoOptionError
 from gi.repository import Gst
 
 from voctocore.lib.config import Config
@@ -15,50 +14,31 @@ class GstAVSource(AVSource):
                          force_num_streams)
 
         self.name = name
-        self.pattern = Config.getTestPattern(name)
-        self.wave = Config.getTestWave(name)
+        self.audio_source = Config.getGstAudioPipe(name)
+        self.video_source = Config.getGstVideoPipe(name)
         self.build_pipeline()
-
-    def port(self):
-        if self.has_video:
-            if self.internal_audio_channels():
-                return "(AV:{}+{})".format(self.pattern, self.wave)
-            else:
-                return "(V:{})".format(self.pattern)
-        else:
-            if self.internal_audio_channels():
-                return "(A:{})".format(self.wave)
-        return "Test"
 
     def num_connections(self):
         return 1
 
+    def port(self):
+        if self.internal_audio_channels():
+            audio_hint=self.audio_source.split()[0]
+        else:
+            audio_hint="(None)"
+        if self.has_video:
+            video_hint=self.video_source.split()[0]
+        else:
+            video_hint="(None)"
+
+        return f"(AV:{audio_hint}+{video_hint})"
+
     def __str__(self):
-        return 'TestSource[{name}] ({pattern}, {wave})'.format(
-            name=self.name,
-            pattern=self.pattern,
-            wave=self.wave
-        )
+        raise
+        return f'GSTSource[{self.name}] ({self.port()})'
 
     def build_audioport(self):
-        # a volume of 0.126 is ~18dBFS
-        return """audiotestsrc
-                      name=audiotestsrc-{name}
-                      do-timestamp=true
-                      freq=1000
-                      volume=0.126
-                      wave={wave}
-                      is-live=true""".format(
-            name=self.name,
-            wave=self.wave,
-        )
+        return self.audio_source
 
     def build_videoport(self):
-        return """videotestsrc
-                      name=videotestsrc-{name}
-                      do-timestamp=true
-                      pattern={pattern}
-                      is-live=true""".format(
-            name=self.name,
-            pattern=self.pattern
-        )
+        return self.video_source
